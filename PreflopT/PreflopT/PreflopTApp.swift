1262ef2f74ec7d003358e6e95f66f4a971ac90b4
Tal Harel <tal20harel@gmail.com>
2026-05-01 16:18:39 +0300

    Add PreflopT Xcode project scaffold (SwiftUI, iOS 17+)

---
+++ PreflopT/PreflopT/PreflopTApp.swift
@@ -0,0 +1,17 @@
+//
+//  PreflopTApp.swift
+//  PreflopT
+//
+//  Created by Harel, Tal on 01/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct PreflopTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PreflopT/PreflopTTests/PreflopTTests.swift
@@ -0,0 +1,16 @@
+//
+//  PreflopTTests.swift
+//  PreflopTTests
+//
+//  Created by Harel, Tal on 01/05/2026.
+//
+
+import Testing
+
+struct PreflopTTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
